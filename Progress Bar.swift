4775bf2ed748f1ef43d5b4cffd157a6144f010e6
Rohan Patel <rpatel5@bsd@220.org>
2025-01-08 09:42:43 -0600

    Created progress bar

---
+++ Progress Bar.swift
@@ -0,0 +1,18 @@
+//
+//  Progress Bar.swift
+//  Country Flag Game
+//
+//  Created by rohan patel on 1/8/25.
+//
+
+import SwiftUI
+
+struct Progress_Bar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Progress_Bar()
+}
